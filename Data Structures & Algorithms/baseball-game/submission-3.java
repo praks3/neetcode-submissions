25e7827342a1156495c1ab3e470e7c88de73233b
Prakhar Srivastava <64420033+praks3@users.noreply.github.com>
2026-08-11 23:18:29 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        int totalScore = 0;
+        for (String op : operations) {
+            if (op.matches("-?\\d+")) {
+                stack.push(Integer.parseInt(op));
+            } else if (op.equals("+")) {
+                int n = stack.size();
+                if (n>=2) {
+                    stack.push(stack.get(n - 1) + stack.get(n - 2));
+                }
+            } else if (op.equals("C")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (op.equals("D")) {
+                if (!stack.isEmpty()) {
+                    int element = stack.peek();
+                    stack.push(element * 2);
+                }
+            }
+        }
+        for (int x : stack) {
+            totalScore += x;
+        }
+        return totalScore;
+    }
+}
